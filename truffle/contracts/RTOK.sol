e19655f857f738e405d6e8a555adf9e3496e2f8b
ESR <82104698+esrdapp@users.noreply.github.com>
2021-12-05 12:15:30 +0000

    Update RTOK.sol

--- truffle/contracts/RTOK.sol
+++ truffle/contracts/RTOK.sol
@@ -4,10 +4,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 // This ERC-20 contract mints the specified amount of tokens to the contract creator.
-contract RTOK is ERC20, ERC20Detailed {
+contract DISPERSE is ERC20, ERC20Detailed {
     constructor(uint256 initialSupply)
         public
-        ERC20Detailed("RTOKEN", "RTOK", 18)
+        ERC20Detailed("DISPERSE", "DISPERSE", 18)
     {
         _mint(msg.sender, initialSupply);
     }
